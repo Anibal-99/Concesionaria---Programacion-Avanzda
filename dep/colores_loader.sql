327a3c742834ea064a2189c1b176be496c26b0e6
Samuel Andrés <samuel.andres@d3sistemas.com.ar>
2022-09-25 19:18:14 -0300

    Agg loaders

---
+++ dep/colores_loader.sql
@@ -0,0 +1,17 @@
+insert into color (nombre) values ('Rojo');
+insert into color (nombre) values ('Verde');
+insert into color (nombre) values ('Azul');
+insert into color (nombre) values ('Gris');
+insert into color (nombre) values ('Negro');
+insert into color (nombre) values ('Dorado');
+insert into color (nombre) values ('Amarillo');
+insert into color (nombre) values ('Lima');
+insert into color (nombre) values ('Rosa');
+insert into color (nombre) values ('Celeste');
+insert into color (nombre) values ('Crema');
+insert into color (nombre) values ('Blanco');
+insert into color (nombre) values ('Naranja');
+insert into color (nombre) values ('Violeta');
+insert into color (nombre) values ('Marron');
+insert into color (nombre) values ('Purpura');
+insert into color (nombre) values ('Verde oscuro');
